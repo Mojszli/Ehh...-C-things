64477ff98b012f7103d367e04020cf55f1fcd4ca
Mojszli <viktor.denes2001@gmail.com>
2025-06-25 00:36:20 +0200

    Add files via upload

---
+++ Variables/Variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::string something = "Hello, World!"; // A string variable
+    std::int number = 42; // An integer variable
+    std::double number = 3.1459; // Can store a lot of decimal places
+    std::float pi = 3.14; // A floating-point variable, cant store as many decimal places as double
+    std::bool isTrue = true; // A boolean variable
+
+    return 0;
+}
